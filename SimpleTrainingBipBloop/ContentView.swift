c1fcae9347978a26051c4824a9aff1b6270e547f
vladukhaDog <vladic.p@gmail.com>
2020-11-20 10:54:38 +0300

    now plays sound, starts a timer

--- SimpleTrainingBipBloop/ContentView.swift
+++ SimpleTrainingBipBloop/ContentView.swift
@@ -6,11 +6,16 @@
 //
 
 import SwiftUI
-
+import AVFoundation
 
 struct ContentView: View {
 	
 	@State var seconds:Int = 0
+	@State var beepSound: AVAudioPlayer?
+	let path = Bundle.main.path(forResource: "beep.mp3", ofType:nil)!
+	let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+	@State var isActive = false
+	
 	
     var body: some View {
 		VStack {
@@ -41,17 +46,40 @@ struct ContentView: View {
 						.padding(.horizontal)
 				}
 			}
-			Spacer()
-			Image(systemName: "minus")
-				.resizable()
-				.scaledToFit()
+			//Spacer()
 			Button(action: {
-				//dogay
+				
+			}) {
+				Text("Random")
+					.font(.system(size: 40, weight: .heavy, design: .default))
+			}
+			
+			Button(action: {
+				self.isActive = true
+				do {
+					let url = URL(fileURLWithPath: path)
+					beepSound = try AVAudioPlayer(contentsOf: url)
+					
+				} catch {
+					print("there was no sound")
+				}
 			}) {
 				Image(systemName: "play.rectangle")
 					.resizable()
 					.scaledToFit()
 					.padding()
+			}
+		}
+		.onReceive(timer) { time in
+			if self.isActive {
+				if self.seconds > 0 {
+					self.seconds -= 1
+				} else {
+					//---------
+					beepSound?.play()
+					//--------
+					self.isActive = false
+				}
 			}
 		}
     }
